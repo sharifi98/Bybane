e6cfd37748411955800ebbcd31429a8af1096409
Hossein Sharifi <47387238+sharifi98@users.noreply.github.com>
2023-06-02 14:10:59 +0200

    Initial Commit

---
+++ Bybane/BybaneApp.swift
@@ -0,0 +1,17 @@
+//
+//  BybaneApp.swift
+//  Bybane
+//
+//  Created by Hossein Sharifi on 02/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct BybaneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
